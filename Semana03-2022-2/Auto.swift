951272182f2e633a4356ee0226a6bef46b89e5a2
alvaro270 <alvarogamer270@gmail.com>
2022-09-09 17:32:21 -0500

    first commiter

--- Semana03-2022-2/Auto.swift
+++ Semana03-2022-2/Auto.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+class Auto {
+    
+    //si no tnenemos contructor , se puede inicializar variables
+    var brand: String = "Toyota"
+    var model: String = "Pickuo"
+    var color: String = "negro"
+    var speed: Float = 60.5
+    
+    func move(moreSpeed: Float){
+        speed += moreSpeed
+        if speed > 160 {
+            self.stop()
+        }
+    }
+    
+    func slowDown(lessSpeed: Float) {
+        if speed > 0 {
+            move(moreSpeed: lessSpeed)
+        }
+    }
+    
+    func stop(){
+        print("Autho stopped")
+        speed = 0.0
+    }
+}
--- Semana03-2022-2/ViewController.swift
+++ Semana03-2022-2/ViewController.swift
@@ -9,11 +9,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    //instanciar auto
+    
+    let auto: Auto = Auto()
+    
+    
+    //referenciar labels de la vista
+    @IBOutlet weak var lblBrand: UILabel!
+    
+    @IBOutlet weak var lblModel: UILabel!
+    
+    @IBOutlet weak var lblColor: UILabel!
+    
+    @IBOutlet weak var lblSpeed: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        /// recordar que esta funcion se ejecuta cuando la pantalla inicia
+        setUpLabel(auto: auto)
+        
     }
-
-
+    ///los actions y func siempre van despues del viewdidload
+    
+    @IBAction func onTapNewCar(_ sender: UIButton) {
+            auto.brand = "Nissan"
+            auto.model = "Frontier"
+            auto.color = "red"
+            auto.speed = 50.3
+            setUpLabel(auto: auto)
+        }
+        
+        @IBAction func onTapAcelerate(_ sender: UIButton) {
+            let newSpeed = Float.random(in: 1...10)
+            auto.move(moreSpeed: newSpeed)
+            setUpLabel(auto: auto)
+        }
+        
+        @IBAction func onTapStop(_ sender: UIButton) {
+            auto.stop()
+            setUpLabel(auto: auto)
+        }
+        
+        
+        @IBAction func onTapSlowDown(_ sender: UIButton) {
+            let newSpeed = Float.random(in: 1...10)
+            auto.slowDown(lessSpeed: newSpeed)
+            setUpLabel(auto: auto)
+        }
+        
+        func setUpLabel(auto: Auto) {
+            lblBrand.text = auto.brand
+            lblModel.text = auto.model
+            lblColor.text = auto.color
+            lblSpeed.text = String(auto.speed)
+        }
+    
 }
 
