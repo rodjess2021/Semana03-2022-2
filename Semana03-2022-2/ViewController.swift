3b62069d8d3249bdcf898549fb1181f3284b0810
alvaro270 <alvarogamer270@gmail.com>
2022-09-09 15:44:53 -0500

    Initial Commit

---
+++ Semana03-2022-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Semana03-2022-2
+//
+//  Created by MAC46 on 9/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
